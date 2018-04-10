0d19a08e7d4001e666851c30bb527de2255625cc
Dave Brown <davebrown.dev@gmail.com>
2018-04-09 21:23:11 -0400

    missed this.

--- client/src/app/bucket-list/bucket-list/bucket-list.component.ts
+++ client/src/app/bucket-list/bucket-list/bucket-list.component.ts
@@ -3,19 +3,20 @@ import { BucketService } from '../../../services/bucket.service';
 import { IBucket } from '../../../models';
 
 @Component({
-  selector: 'app-bucket-list',
-  templateUrl: './bucket-list.component.html',
-  styleUrls: ['./bucket-list.component.scss']
+    selector: 'app-bucket-list',
+    templateUrl: './bucket-list.component.html',
+    styleUrls: ['./bucket-list.component.scss']
 })
 export class BucketListComponent implements OnInit {
 
-  constructor(public bucketService: BucketService) { }
+    public bucketItems: Array<IBucket>;
+    constructor(public bucketService: BucketService) { }
 
-  ngOnInit() {
-    this.bucketService.getMyList().subscribe((items: Array<IBucket>) =>{
-        
-        console.dir(items);
-    })
-  }
+    ngOnInit() {
+        this.bucketService.getMyList().subscribe((items: Array<IBucket>) => {
+            this.bucketItems = items;
+            console.dir(items);
+        })
+    }
 
 }
