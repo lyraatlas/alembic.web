76e54f4f64f5e83f5a16d47bb0a2356f786fd53d
Dave Brown <davebrown.dev@gmail.com>
2018-08-10 23:03:56 -0400

    checking other buckets to see if we can delete the imagess.

--- server/controllers/bucket.controller.ts
+++ server/controllers/bucket.controller.ts
@@ -1,14 +1,13 @@
-import { IBucketDoc, Bucket, ITokenPayload, IBaseModel, IBucket, IBaseModelDoc } from '../models';
-import { Router, Request, Response, RequestParamHandler, NextFunction, RequestHandler } from 'express';
-import mongoose = require('mongoose');
-import { Schema, Model, Document } from 'mongoose';
-import { BaseController } from './base/base.controller';
+import { NextFunction, Request, Response } from 'express';
 import { CONST } from '../constants';
-import { BucketRepository } from "../repositories";
-import { OwnershipType } from "../enumerations";
-import { Likeable } from './base/likeable.mixin';
+import { IBucketDoc, IBucketItemDoc } from '../models';
+import { BucketItemRepository, BucketRepository } from "../repositories";
+import { BaseController } from './base/base.controller';
 import { Commentable } from './base/commentable.mixin';
 import { ImageControllerMixin } from './base/images.controller.mixin';
+import { Likeable } from './base/likeable.mixin';
+import { BucketItemController } from './bucket-item.controller';
+import mongoose = require('mongoose');
 
 export class BucketControllerBase extends BaseController {
 
@@ -19,6 +18,8 @@ export class BucketControllerBase extends BaseController {
     public isOwnershipRequired = true;
 
     public repository = new BucketRepository();
+    public bucketItemRepository = new BucketItemRepository();
+    public bucketItemController =  new BucketItemController();
 
     constructor() {
         super();
@@ -29,8 +30,41 @@ export class BucketControllerBase extends BaseController {
         return Bucket;
     }
 
-    public async preDestroyHook(request: Request, response: Response, next: NextFunction, bucket:IBucketDoc):Promise<IBucketDoc>{
-        return await BucketController.destroyImages(bucket);
+    public async preDestroyHook(request: Request, response: Response, next: NextFunction, bucket: IBucketDoc): Promise<IBucketDoc> {
+        try {
+
+            // Before we can destroy the images related to this bucket's items. 
+            // keep in mind this bucket item might have been added to someone else's bucket.
+            // so we can only clean up the images on this guy if he's not a part of any other bucket.
+            // this is generally what querying inside an array looks like. db.inventory.find( { tags: "red" } )
+            if (bucket.bucketItems && bucket.bucketItems.length > 0) {
+                // For each of the bucket items on this bucket.
+                for (let i = 0; i < bucket.bucketItems.length; i++) {
+
+                    const bucketItem = bucket.bucketItems[i] as IBucketItemDoc;
+
+                    // We need to check to see if this bucket item exists on any other buckets first. 
+                    let otherBuckets = await this.repository.query({
+                        bucketItems: bucketItem._id
+                    }, null, null);
+
+                    // Now if we have other buckets, we're not going to delete the images,... but we'll delete them if there's no other buckets.
+                    if (!otherBuckets || otherBuckets == null || otherBuckets.length <= 1) {
+                        // here we know that this bucket item doesn't exist in anyone elses bucket.
+                        // so we can go ahead and delete the images off of it. 
+                        await BucketItemController.destroyImages(await this.bucketItemRepository.single(bucketItem._id));
+
+                        // For now we're not going to go an delete the bucket item.
+                        // it's a pain, and it's probably not that big of a deal.  In theory we can write something that will spit out all the orphaned
+                        // bucket items and delete those. 
+                    }
+                }
+            }
+
+            return await BucketController.destroyImages(bucket);
+
+        } catch (err) { next(err); }
+
     }
 
     public async preSendResponseHook(Bucket: IBucketDoc): Promise<IBucketDoc> {
